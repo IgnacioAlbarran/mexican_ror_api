cc4a554f87c3fb42b913d27f842d3b997680fe58
Ignacio Albarrán <ignacioalbarran@emergia.net>
2020-07-11 13:57:44 +0200

    small validation refactoring

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,4 +1,7 @@
 class Venue < ApplicationRecord
+  HOURS_REGEXP = /(\d{2}\:\d{2}\-\d{2}\:\d{2}\,){6}\d{2}\:\d{2}\-\d{2}\:\d{2}/
+  URL_REGEXP = /(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?/ix
+
   validates :name, presence: true, uniqueness: true
   validates :address_line_1, presence: true
   validates :lat, presence: true
@@ -8,8 +11,9 @@ class Venue < ApplicationRecord
   validates :category_id_b, presence: true
   validate :category_id_b_is_valid
   validates :closed, presence: true
-  validates :hours, format: { with: /(\d{2}\:\d{2}\-\d{2}\:\d{2}\,){6}\d{2}\:\d{2}\-\d{2}\:\d{2}/,
+  validates :hours, format: { with: HOURS_REGEXP,
     message: 'Format must be like: 10:00-22:00,10:00-22:00,10:00-22:00,10:00-22:00,10:00-22:00,11:00-18:00,11:00-18:00' }
+    validates :website, format: { with: URL_REGEXP, message: 'You provided invalid URL' }
 
   VALID_CATEGORIES_ID_A = 1000..1200
   VALID_CATEGORIES_ID_B = 2000..2200
