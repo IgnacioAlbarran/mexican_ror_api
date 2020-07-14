02c6a5cf7f4970b7693ceaf6ab21b485a17555e7
Ignacio Albarrán <ignacioalbarran@emergia.net>
2020-07-14 22:51:36 +0200

    fix URL for platform a

--- lib/platforms.rb
+++ lib/platforms.rb
@@ -1,7 +1,7 @@
 require 'httparty'
 
 class Platforms
-  URL_A = 'https://rokuapp.com/platform_a/venue?api_key=f4ac0bb0f8bdad55977df17d72835d82'
+  URL_A = 'https://rails-code-challenge.herokuapp.com/platform_a/venue?api_key=f4ac0bb0f8bdad55977df17d72835d82'
   URL_B = 'https://rails-code-challenge.herokuapp.com/platform_b/venue?api_key=f4ac0bb0f8bdad55977df17d72835d82'
   URL_C = 'https://rails-code-challenge.herokuapp.com/platform_c/venue?api_key=f4ac0bb0f8bdad55977df17d72835d82'
 
